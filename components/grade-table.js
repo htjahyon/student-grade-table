4f953d96fd9473903df5a68026db957e2415b9e4
Hanli Tjahyono <hansmans326113@aol.com>
2020-11-21 13:55:39 -0800

    look more like wireframe

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,6 +4,7 @@ class GradeTable {
   }
   updateGrades(grades) {
     var tbodyElement = document.querySelector("tbody");
+    var totalGrade = 0;
     for(var i = 0; i < grades.length; i++) {
     var rowElement = document.createElement("tr");
     tbodyElement.appendChild(rowElement);
@@ -13,7 +14,10 @@ class GradeTable {
       studentElement.textContent = grades[i].name;
       courseElement.textContent = grades[i].course;
       gradeElement.textContent = grades[i].grade;
+      totalGrade += grades[i].grade;
       rowElement.append(studentElement, courseElement, gradeElement);
     }
+    var avgGradeElement = document.querySelector(".avg-grade");
+    avgGradeElement.textContent = totalGrade / grades.length;
   }
 }
