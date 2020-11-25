5319092fae44bf7aceeec2230160d4de590cbc20
Hanli Tjahyono <hansmans326113@aol.com>
2020-11-24 21:18:57 -0800

    add a gradeform component

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var tableElement = document.querySelector(".table");
 var gradeTable = new GradeTable(tableElement);
 var headerElement = document.querySelector(".avg-grade");
 var pageHeader = new PageHeader(headerElement);
-var app = new App(gradeTable, pageHeader);
+var form = document.querySelector("form");
+var gradeForm = new GradeForm(form);
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
